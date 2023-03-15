68d98b8efcc4879283c42a528e885e3025d83bf9
luanhcastro <luanhcvalese@gmail.com>
2023-03-15 15:16:26 -0300

    fixing responsiviness

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ import {
   Input,
 } from "antd";
 import data from "./dataset/data.json";
-import { DownloadOutlined, CloseCircleOutlined, SearchOutlined } from "@ant-design/icons";
+import {
+  DownloadOutlined,
+  CloseCircleOutlined,
+  SearchOutlined,
+} from "@ant-design/icons";
 import background from "./background.jpg";
 import logo from "./images/logo.png";
 
@@ -40,105 +44,112 @@ function App() {
   return (
     <Row class="body" style={{ display: "flex", justifyContent: "center" }}>
       <Col
-        style={{ display: "flex", justifyContent: "center", marginTop: 80, justifyItems: "center" }}
-        span={24}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 80,
+        }}
+        // xs={8}
+        sm={16}
+        md={24}
       >
         <img class="logo" alt="Logo" src={logo} />
       </Col>
 
       {!visible ? (
         <>
-          <Col span={24} style={{ display: "flex", justifyContent: "center" }}>
+          <Col
+            // xs={8}
+            sm={16}
+            md={24}
+            style={{ display: "flex", justifyContent: "center" }}
+          >
             <Input
               style={{ width: 800, marginTop: 100 }}
               onChange={(e) => filterData(e.target.value)}
               allowClear={true}
               placeholder="Search for molecules"
-              suffix={<SearchOutlined style={{ color:"green", size:"large"}}/>}
+              suffix={
+                <SearchOutlined style={{ color: "green", size: "large" }} />
+              }
               // enterButton="Search"
               size="large"
             />
           </Col>
-          <Col span={24} style={{ display: "flex", justifyContent: "center" }}>
-            <List
-              class="list"
-              grid={{ gutter: 10 }}
-              dataSource={filteredData}
-              // pagination={{
-              //   onChange: (page) => {
-              //     console.log(page);
-              //   },
-              //   align: "center",
-              //   showTotal: false,
-              //   pageSize: 4,
-              //   size: "small",
-              //   simple: true,
-              // }}
-              style={{
-                marginTop: "30px",
-                display: "flex",
-                justifySelf: "center",
-                marginLeft: 32,
-                justifyContent: "center",
-              }}
-              renderItem={(item) => (
-                <List.Item>
-                  <Col span={6}>
-                    {" "}
-                    <div onClick={() => showCard(item)}>
-                      <Card
-                        hoverable
-                        bordered
-                        style={{ width: 350, marginTop: 30 }}
-                        cover={<img alt="Logo" src={item.Link} />}
-                      >
-                        <Divider />
-                        <p>
-                          <Meta
-                            title={item.Compound}
-                            description={<div></div>}
-                          />
-                        </p>
-                      </Card>
-                    </div>
-                  </Col>
-                </List.Item>
-              )}
-            />
+          <Col
+            // xs={8}
+            sm={16}
+            md={24}
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <Row
+              gutter={15}
+              style={{ display: "flex", justifyContent: "center" }}
+            >
+              {filteredData.map((item) => (
+                <Col>
+                  <div onClick={() => showCard(item)}>
+                    <Card
+                      hoverable
+                      bordered
+                      style={{ width: 350, marginTop: 30 }}
+                      cover={<img alt="Logo" src={item.Link} />}
+                    >
+                      <Divider />
+                      <p>
+                        <Meta title={item.Compound} />
+                      </p>
+                    </Card>
+                  </div>
+                </Col>
+              ))}
+            </Row>
           </Col>
         </>
       ) : (
-        <Card
-          extra={
-            <Button type="link" danger onClick={() => setVisible(false)}>
-              <CloseCircleOutlined />
-            </Button>
-          }
-          title={item.Compound}
-          style={{ marginTop: "10px", marginBottom: "100px", width: "70%" }}
+        <Col
+          // xs={8}
+          sm={16}
+          md={24}
+          style={{ display: "flex", justifyContent: "center" }}
         >
-          <p>
-            <Descriptions bordered column={1}>
-              <Descriptions.Item label={<img src={item.Link} alt="imagem" />}>
-                {item.Compound}
-              </Descriptions.Item>
-              <Descriptions.Item label="Name">
-                {item.Compound}
-              </Descriptions.Item>
-              <Descriptions.Item label="Smiles">
-                {item.Smiles}
-              </Descriptions.Item>
-              <Descriptions.Item label="Mol weight">
-                {item.Molweight}
-              </Descriptions.Item>
-              <Descriptions.Item label="MOL">
-                <Button type="primary" icon={<DownloadOutlined />}>
-                  Download Mol
-                </Button>
-              </Descriptions.Item>
-            </Descriptions>
-          </p>
-        </Card>
+          <Card
+            extra={
+              <Button type="link" danger onClick={() => setVisible(false)}>
+                <CloseCircleOutlined />
+              </Button>
+            }
+            title={item.Compound}
+            style={{
+              marginTop: "10px",
+              marginBottom: "100px",
+              width: "70%",
+              minWidth: "700px",
+            }}
+          >
+            <p>
+              <Descriptions sm={16} md={24} bordered column={1}>
+                <Descriptions.Item label={<img src={item.Link} alt="imagem" />}>
+                  {item.Compound}
+                </Descriptions.Item>
+                <Descriptions.Item label="Name">
+                  {item.Compound}
+                </Descriptions.Item>
+                <Descriptions.Item label="Smiles">
+                  {item.Smiles}
+                </Descriptions.Item>
+                <Descriptions.Item label="Mol weight">
+                  {item.Molweight}
+                </Descriptions.Item>
+                <Descriptions.Item label="MOL">
+                  <Button type="primary" icon={<DownloadOutlined />}>
+                    Download Mol
+                  </Button>
+                </Descriptions.Item>
+              </Descriptions>
+            </p>
+          </Card>
+        </Col>
       )}
     </Row>
   );
